c4f13c6af72ab68d0c5c527bec327b8759a7adf9
yuyurun <yuyu1.run@icloud.com>
2020-09-05 18:02:22 +0900

    add 05

---
+++ src/ch01/ans05.py
@@ -0,0 +1,19 @@
+def create_ngram(text, n=2, mode='str'):
+    """
+    n-gramを作る
+
+    Args:
+        text(str) : 対象となる文
+        n(int) : n-gramのn
+        mode(str) : strかword 
+    """
+    if mode == 'str':
+        l = [s for s in text]
+    else:
+        l = [s for s in text.split(' ')]
+    return [''.join(l[i:i+n]) for i in range(len(l) - n + 1)]
+
+
+a = 'I am an NLPer'
+print(create_ngram(a))
+print(create_ngram(a, mode='word'))
