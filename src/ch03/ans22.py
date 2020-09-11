26543a7ef846c2a77b3c97bb22a774964c26e50e
yuyurun <yuyu1.run@icloud.com>
2020-09-11 21:17:25 +0900

    add 22

---
+++ src/ch03/ans22.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from ans20 import extract_ing
+
+if __name__ == '__main__':
+    text = extract_ing()
+    for line in text.split('\n'):
+        if '[[Category' in line:
+            print(line.replace(
+                '[[Category:', '').replace(']]', '').replace('|*', ''))
