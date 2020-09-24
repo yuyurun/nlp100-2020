fb213ab10b87d520351758ddb6dd7d74854cce53
yuyurun <yuyu1.run@icloud.com>
2020-09-24 17:16:24 +0900

    add 28

---
+++ src/ch03/ans28.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from ans20 import extract_ing
+
+import re
+
+if __name__ == '__main__':
+    text = extract_ing()
+
+    lines = text.split('\n')
+    pattern = re.compile('\|(.*?)\s=\s*(.*)')
+    pattern2 = re.compile("'+")
+    pattern3 = re.compile('\[\[(.+\||)(.+?)\]\]')
+    pattern4 = re.compile('\{\{(.+?)\}\}')
+
+    ans = {}
+    for line in lines:
+        r = re.search(pattern, line)
+        if r:
+            rr = pattern3.sub(r'\2', r[2])
+            rr = pattern4.sub(r'\1', rr)
+            ans[r[1]] = pattern2.sub('', rr)
+
+    print(ans)
