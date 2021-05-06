a697aec4b2108b28c9d6e17d761264ad3ae7c71a
yuyurun <yuyu1.run@icloud.com>
2021-05-06 21:51:04 +0900

    add ans31

---
+++ src/ch04/ans31.py
@@ -0,0 +1,28 @@
+import itertools
+
+def parse_mecab(sen):
+    l_sen = []
+    for line in sen.split('\n'):
+        if line == '':
+            return l_sen
+
+        (surface, attr) = line.split('\t')
+        attr = attr.split(',')
+        l_dic = {
+            'serface': surface,
+            'base': attr[6],
+            'pos': attr[0],
+            'pos1': attr[1] 
+        }
+        l_sen.append(l_dic)
+        
+
+fname = '../../data/neko.txt.mecab'
+with open(fname) as f:
+    lines = f.read().split('EOS\n')
+
+lines = list(filter(lambda x: x != '', lines))
+results = [parse_mecab(line) for line in lines]
+
+dousi = [[r['serface'] for r in result if r['pos'] == '動詞'] for result in results]
+print(list(itertools.chain.from_iterable(dousi)))
